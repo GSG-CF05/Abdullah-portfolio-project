a781783ef0084fb1bb9ca76c17a9a56c40a8ef2d
AbdullahLaham <abed26194@gmail.com>
2022-02-03 16:48:06 +0200

    create the About section and adding it's details relate #5

--- file.js
+++ file.js
@@ -1,23 +1,24 @@
-let p = document.querySelector('h3 p');
-let x = 'Front End Devoloper.';
+// let p = document.querySelector('h3 p');
+// let x = '';
 
-let text = '';
-setInterval(()=> {
-    let i = 0;
-    var interval = setInterval(() => {
-        text += (x.split(""))[i];
-        if (i < x.length-1) {
-            p.innerHTML = text;
-            i++;
-        } else {
-            p.innerHTML = '';
-            text = '';
-            clearInterval(interval);
-        }
+// let text = '';
+// setInterval(()=> {
+//     let i = 0;
+//     var interval = setInterval(() => {
+//         text += (x.split(""))[i];
+//         if (i < x.length-1) {
+//             p.innerHTML = text;
+//             i++;
+            
+//         } else {
+//             p.innerHTML = '';
+//             text = '';
+//             clearInterval(interval);
+//         }
        
         
-    }, 200);
-}, 4400);
+//     }, 200);
+// }, 4400);
 
     
 
